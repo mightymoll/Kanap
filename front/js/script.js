8ff222d3e63001aa6a49e9f7791a535d23fbb5a7
mightymoll <109845481+mightymoll@users.noreply.github.com>
2022-10-19 12:42:59 +0200

    Node server setup and .js for index
    - ran npm install to setup node server
    - created script.js to fetch product info from API and modify the DOM
    
    P5 of OpenClassrooms web dev program

---
+++ front/js/script.js
@@ -0,0 +1,38 @@
+// get data from API
+/* if API responds, put data in .json
+/* then execute function 'getProducts' with data from API
+/* if API does not respond, place error 404 msg in <h1> tag of html
+*/
+
+let API = 'http://localhost:3000/api/products'
+
+fetch(API)
+  .then(data => data.json())
+  .then(data => getProducts(data))
+
+  .catch((error) => {
+    document.getElementsByClassName(".titles").innerHTML = "<h1>error 404</h1>";
+    console.log("error 404, could not connect to API" + error);
+  });
+
+// use data from API to change DOM of index.html 
+/* create variable 'items' to be used in the 'items' section of index.html
+/* use articles from the API to overwrite DOM elements
+/* link to product page using product id 
+/* product image and alt info
+/* product name and description 
+*/
+
+function getProducts(data) {
+  let items = document.querySelector("#items");
+  for (let article of data) {
+    items.innerHTML += (`<a href="./product/${article._id}.html">
+    <article>
+      <img src="${article.imageUrl}" alt="${article.altTXT}"/>
+      <h3>${article.name}</h3>
+      <p>${article.description}</p>
+    </article>
+    </a>`);
+  }
+}
+
