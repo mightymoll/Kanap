42b5466250edde929110195be0c579b3edefdf6f
mightymoll <109845481+mightymoll@users.noreply.github.com>
2022-10-21 10:54:31 +0200

    'cart item' created
    - record customer choice of both product color and quantity
    - if both choices have values, place 'cart item' in localstorage
    - if incomplete, prompt user to make selections
    
    P5 of OpenClassrooms web developer program

--- front/js/product.js
+++ front/js/product.js
@@ -7,17 +7,16 @@ let searchUrl = new URLSearchParams(window.location.search)
 let productId = searchUrl.get('_id');
 console.log(productId);
 
-// Get product information for use in DOM manipulation
-/* create url that returns only the array associated with the product id
-/* log result in console for verification
-
-/* fetch product information from API
-/* create .json and log response
-/* then run 'productDisplay' function
-/* if no response, place 404 error in h1 title of page */
+//* create url that returns only the array associated with the product id
+/* log result in console for verification */
 
 let productInfo = "http://localhost:3000/api/products/" + productId;
 console.log(productInfo);
+
+// get product array from API
+/* convert to .json and log response
+/* then run 'productDisplay' function
+/* if no response, place 404 error in h1 title of page */
 
 fetch(productInfo)
   .then((response) => response.json())
@@ -28,37 +27,35 @@ fetch(productInfo)
     console.log("error 404, could not connect to API" + error);
   });
 
-
+// populate DOM with product info from API
 function productDetails(product) {
   const item = product; //make 'product' object iterable
   console.log(item); //log results for verification
-  // photo //
+
+  // product image //
   let img = document.createElement("img"); //create img in DOM w/product values
   img.src = item.imageUrl;
   img.alt = item.altTxt;
   document.getElementsByClassName('item__img')[0].appendChild(img); //add img element to DOM
 
-  // name //
+  // product name //
   let name = document.getElementById('title');
   name.innerHTML += (`<h1>${item.name}</h1>`);
 
-  // price //
+  // product price //
   let price = document.getElementById('price');
   price.innerHTML += (`<span>${item.price}</span>`);
 
-  // description //
+  // product description //
   let productDesc = document.getElementById('description');
   productDesc.innerHTML += (`<p>${item.description}</p>`);
 
-  // color select //
+  // product select color options //
+  /* Until number of colors (i) = 0,
+  /* add 1 new <option> element to DOM with values from 'colors' */
+
   let colors = item.colors;
   console.log(colors); //for verification purposes
-
-  // Until number of colors = 0
-  /* continue to add <option> elements to DOM 
-  /* with values from 'colors' array */
-
-  //i represents the variable 'colors'
 
   for (let i = 0; i < colors.length; i++) {
     let colorName = colors[i];
@@ -67,5 +64,37 @@ function productDetails(product) {
     option.value = colorName;
     const optionAdd = document.getElementById('colors'); //location of select dropdown
     optionAdd.appendChild(option);
+  }
+}
+
+// log customer choices
+
+// button - set click event
+/* when button is clicked, run function 'addToCart' */
+let button = document.getElementById('addToCart');
+button.addEventListener("click", addToCart);
+
+// create cart item (product w/customer selections) map to store locally
+/* retrieve selected color and item quantity from DOM
+/* create map 'cartItem' (product id, color choice, quantity)
+/* if color and quantity are both complete, set cartItem in localstorage (stringified for easy retrieval)
+/* if not, prompt user to check that they have selected a color and entered a quantity */
+
+function addToCart() {
+  let selectedColor = document.getElementById('colors').selectedOptions[0].value;
+  let itemQuantity = document.getElementById('quantity').value;
+
+  let cartItem = new Map();
+  cartItem.set('product', productId)
+    .set('color', selectedColor)
+    .set('quantity', itemQuantity);
+
+  if (selectedColor && itemQuantity > 0) {
+    localStorage.setItem('cartItem', JSON.stringify(Map))
+    alert('Article ajouté au panier avec succès');
+  }
+  else {
+    console.log("product options incomplete");
+    alert('Assurez-vous d\'avoir choisi une couleur et une quantité pour ce produit')
   }
 }
