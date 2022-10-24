b0cadd06312ca1e5ae56026995816b6d732aca23
mightymoll <109845481+mightymoll@users.noreply.github.com>
2022-10-24 20:17:36 +0200

    cart item data refinement
    - previous code did not account for different product/color combinations (just id)
    - flattened & rebuilt catalog array to include all possible product/color combinations
    - new filtering reduces catalog to only relevant data/items in cart
    
    *DOM manipulation in progress
    
    P5 of OpenClassrooms web developer program

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,26 +1,66 @@
-
 // get all product data from API
 fetch('http://localhost:3000/api/products')
   .then((response) => response.json())
-  .then((allProducts) => cartProductData(allProducts))
+  .then((allProducts) => cartItems(allProducts))
 
   .catch((error) => {
     document.getElementById("cartAndFormContainer").innerHTML = "<h1>error 404</h1>";
     console.log("error 404, could not connect to API" + error);
   });
 
-// filter API product data using cart item id's
-/* includes only data for products currently in the cart */
-
-function cartProductData(allProducts) {
-
-  let catalog = allProducts
-  console.log(catalog)
-
-  var cart = JSON.parse(localStorage.getItem('cart'));
+// filter API product data ('catalog') using product/color combinations in cart
+/* returns data for use in DOM manipulation */
+function cartItems(allProducts) {
+  let cart = JSON.parse(localStorage.getItem('cart'));
   console.log(cart)
 
-  const cartItems = catalog.filter(cartItem => cart.some(product =>
-    cartItem._id === product._id));
-  console.log(cartItems)
+  const catalog = allProducts.flatMap(product => {
+    return product.colors.map(color => {
+      return {
+        id: product._id,
+        color: color,
+        name: product.name,
+        price: product.price,
+        imageUrl: product.imageUrl,
+        description: product.description,
+        altTxt: product.altTxt,
+      };
+    });
+  });
+  console.log(catalog)
+
+  const cartProdData = catalog.filter(function (catalog_el) {
+    return cart.filter(function (cart_el) {
+      return ((cart_el.id === catalog_el.id) && (cart_el.color === catalog_el.color));
+    }).length != 0
+  });
+  console.log(cartProdData)
+}
+
+/* UNDER CONSTRUCTION */
+function displayCartItems(cartProdData) {
+  let cartItem = document.createElement("ARTICLE");
+
+  for (let article of cartProdData) {
+    cartItem.innerHTML += (`<article class="cart__item" data-id="${article.id}" data-color="${article.color}">  
+    <div class="cart__item__img">
+        <img src="${article.imageUrl}" alt="${article.altTxt}">
+      </div>
+      <div class="cart__item__content">
+        <div class="cart__item__content__description">
+          <h2>${article.name}</h2>
+          <p>${article.color}</p>
+          <p>${article.price} €</p>
+        </div>
+        <div class="cart__item__content__settings">
+          <div class="cart__item__content__settings__quantity">
+            <p>Qté : </p>
+            <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="42">
+          </div>
+          <div class="cart__item__content__settings__delete">
+            <p class="deleteItem">Supprimer</p>
+          </div>
+        </div>
+      </div>`);
+  } document.getElementById('cart__items')[0].appendChild(cartItem);
 }
