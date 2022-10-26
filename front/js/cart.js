3748717d918faecd20f46c93e71aecd74ae1ebb7
mightymoll <109845481+mightymoll@users.noreply.github.com>
2022-10-26 16:27:48 +0200

    cart page refactor
    - multiple fetch; get product info from API product page based on id#
      - previous method of filtering all API data would be inefficient in larger-scale projects
    - added user functionality to change input qty/delete items
    
    * next step: log cart item changes in localstorage, calculate cart total
    
    P5 of OpenClassrooms web developer program

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,75 +1,105 @@
-// get all product data from API
-fetch('http://localhost:3000/api/products')
-  .then((response) => response.json())
-  .then((allProducts) => cartItems(allProducts))
+// get cart items from local storage
+var cart = JSON.parse(localStorage.getItem('cart'));
+console.log(cart);
+
+var productIds = cart.map(product => product.id)
+console.log(productIds);
+
+let baseURL = "http://localhost:3000/api/products";
+let productUrls = productIds.map((productId) => {
+  return `${baseURL}/${productId}`
+})
+console.log(productUrls);
+
+let requestData = productUrls.map(url => fetch(url));
+
+Promise.all(requestData)
+  .then(responses => {
+    return responses;
+  })
+  .then(responses => Promise.all(responses.map(r => r.json())))
+  .then(productData => displayCartItems(productData))
 
   .catch((error) => {
     document.getElementById("cartAndFormContainer").innerHTML = "<h1>error 404</h1>";
     console.log("error 404, could not connect to API" + error);
   });
 
-// filter API product data ('catalog') using product/color combinations in cart
-/* returns data for use in DOM manipulation */
-function cartItems(allProducts) {
-  const cart = JSON.parse(localStorage.getItem('cart'));
-  console.log(cart);
-
-  const catalog = allProducts.flatMap(product => {
-    return product.colors.map(color => {
-      return {
-        id: product._id,
-        color: color,
-        name: product.name,
-        price: product.price,
-        imageUrl: product.imageUrl,
-        description: product.description,
-        altTxt: product.altTxt,
-      };
+function displayCartItems(productData) {
+  let cartItems = [];
+  for (let i = 0; i < cart.length; i++) {
+    cartItems.push({
+      ...cart[i],
+      ...productData[i]
     });
-  });
-  console.log(catalog)
+  }
 
-  let itemQty = cart.map(function (item) {
-    return {
-      qty: item.qty
-    };
-  });
-  console.log(itemQty)
-
-  let cartItems = catalog.filter(function (catalog_el) {
-    return cart.filter(function (cart_el) {
-      return ((cart_el.id === catalog_el.id) && (cart_el.color === catalog_el.color));
-    }).length != 0
-  });
   console.log(cartItems)
 
-  let addQty = Object.assign(...cartItems, ...itemQty);
-  console.log(addQty)
-
-  let cartItem = document.querySelector("#cart__items");
-  for (let product of cartItems) {
-    cartItem.innerHTML += (`
-      <article class="cart__item" data-id="${product.id}" data-color="${product.color}">  
+  for (let item of cartItems) {
+    let cartDisplay = document.querySelector("#cart__items");
+    cartDisplay.innerHTML += (`
+      <article class="cart__item" data-id="${item.id}" data-color="${item.color}">  
       <div class="cart__item__img">
-        <img src="${product.imageUrl}" alt="${product.altTxt}">
+        <img src="${item.imageUrl}" alt="${item.altTxt}">
       </div>
       <div class="cart__item__content">
         <div class="cart__item__content__description">
-          <h2>${product.name}</h2>
-          <p>${product.color}</p>
-          <p>${product.price} €</p>
+          <h2>${item.name}</h2>
+          <p>${item.color}</p>
+          <p>${item.price} €</p>
         </div>
         <div class="cart__item__content__settings">
           <div class="cart__item__content__settings__quantity">
             <p>Qté : </p>
-            <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${product.qty}">
+            <input type="number" class="itemQuantity" name="itemQuantity" data-quantity="" min="1" max="100" value="${item.qty}">
           </div>
           <div class="cart__item__content__settings__delete">
             <p class="deleteItem">Supprimer</p>
           </div>
         </div>
       </div>
-    </article>`);
+    </article> `)
+  }
+
+  // set variable for existing cart to compare changes to
+  const product = document.querySelector('.cart__item');
+  let datasetId = product.dataset.id
+  let datasetColor = product.dataset.color
+
+  var itemInCart = cart.find(function (cartItem) {
+    return (cartItem['_id'] === datasetId && cartItem['color'] === datasetColor)
+  })
+
+  //remove item when 'supprimer' is clicked
+  var deleteItems = document.getElementsByClassName("deleteItem")
+  for (let i = 0; i < deleteItems.length; i++) {
+    var supprimer = deleteItems[i]
+    supprimer.addEventListener('click', removeItem)
+  }
+
+  function removeItem(event) {
+    var clicked = event.target
+    clicked.closest('article').remove()
+    /*updateCartTotal()*/
+  }
+
+  // update item quantity if changed in input field
+  var quantityInputs = document.getElementsByClassName("itemQuantity")
+  for (let i = 0; i < quantityInputs.length; i++) {
+    var inputQty = quantityInputs[i]
+    inputQty.addEventListener('change', updateQty)
+  }
+
+  function updateQty(event) {
+    var newQty = event.target.valueAsNumber
+
+    if (isNaN(newQty) || newQty >= 101 || newQty <= 0) {
+      alert('Merci d\'entrer une quantité entre 1 et 100 ou cliquez sur \'Supprimer\' pour le retirer de votre panier')
+    }
+    if (itemInCart && (itemInCart['qty'] != newQty)) {
+      itemInCart['qty'] += newQty
+    }
+    /*updateCartTotal()*/
   }
 }
-
