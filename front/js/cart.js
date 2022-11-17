5a0d58d4568182d2d373ca5c6bd2f6d021749c93
mightymoll <109845481+mightymoll@users.noreply.github.com>
2022-11-17 23:58:33 +0100

    cart.js reconfig
    - simplified cart data procurement
    - form validation function updated: old parameter allowed invalid inputs through
    
    - bug: event listener's not firing
    
    P5 for OpenClassrooms web developer program

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,44 +1,30 @@
-// Global Variables
+// Global Variables for Cart Display //
 let cart = JSON.parse(localStorage.getItem('cart'))
 
 let totalQty = 0;
 let totalPrice = 0;
 
 let header = document.querySelector("h1")
-let cartList = document.getElementById("cart__items")
+const itemDisplay = document.querySelector('#cart__items')
 let articlesTotal = document.getElementById("totalQuantity")
 let priceTotal = document.getElementById("totalPrice")
 
-getCart().then(displayCart())
-
-async function getCart() {
-  if (!cart || cart.length == 0) {
-    header.innerHTML = (`Votre panier est vide<br><a href=./index.html style=font-size:18px;>trouvez un produit que vous allez aimer</a>`)
-    return console.log('cart is empty or does not exist');
-  }
-  else {
-    await createCartItems()
-    console.log('cart item data fetched')
-  }
-  return cart
-}
-
-async function createCartItems() {
-  for (let item of cart) {
-    let productData = await getProductData(item.id)
-    item['imageUrl'] = productData.imageUrl;
-    item['altTxt'] = productData.altTxt;
-    item['name'] = productData.name;
-    item['price'] = productData.price;
-  }
+console.log(cart)
+if (!cart || cart.length == 0) {
+  header.innerHTML = (`Votre panier est vide<br><a href=./index.html style=font-size:18px;>trouvez un produit que vous allez aimer</a>`)
+  console.log('cart is empty or does not exist');
 }
 
 async function displayCart() {
-  await getCart()
-  console.log(cart)
-  for (let item of cart) {
-    let cartListItem = document.createElement('article')
-    cartListItem.innerHTML = (`<article class="cart__item" data-id="${item.id}" data-color="${item.color}">  
+  for (i = 0; i < cart.length; i++) {
+    const productData = await getProductData((cart[i].id))
+    const item = { ...cart[i], ...productData }
+    const itemCost = parseInt(item.price * item.qty)
+
+    totalQty += item.qty
+    totalPrice += itemCost
+
+    itemDisplay.innerHTML += (`<article class="cart__item" data-id="${item.id}" data-color="${item.color}">  
       <div class="cart__item__img">
         <img src="${item.imageUrl}" alt="${item.altTxt}">
       </div>
@@ -58,56 +44,35 @@ async function displayCart() {
           </div>
         </div>
       </div>
-    </article>`);
-    cartList.appendChild(cartListItem);
-
-    totalQty += item.qty
-    totalPrice += (item.qty * item.price)
-  };
-  console.log(totalPrice)
-  console.log(totalQty)
-
-  articlesTotal.innerHTML = totalQty
-  priceTotal.innerHTML = new Intl.NumberFormat().format(totalPrice)
-}
-
-async function calcCartTotals() {
-  await createCartItems()
-  for (let item of cart) {
-    let itemCost = item.qty * item.price
-
-  }
-  totalQty += item.qty
-  totalPrice += itemCost
-  console.log(totalQty, totalPrice)
-}
-
-
-async function getProductData() {
-  for (let item of cart) {
-    return fetch("http://localhost:3000/api/products/" + item.id)
-      .then(function (res) {
-        return res.json();
-      })
-      .catch((error) => {
-        totalQty = '?';
-        totalPrice = '?';
-        header.innerHTML = (`Sacre Bleu!<p style=font-size:18px;>une erreur est survenue<br>merci de revenir plus tard</p></h1>`)
-        console.log("could not connect to item's API" + error);
-      })
+    </article>`)
+    articlesTotal.innerHTML = totalQty
+    priceTotal.innerHTML = new Intl.NumberFormat().format(totalPrice)
   }
 }
 
-async function eventListeners() {
-  document.querySelectorAll('.itemQuantity').forEach(input => {
-    input.addEventListener('change', updateQty)
-  });
-
-  document.querySelectorAll(".deleteItem").forEach(p => {
-    p.addEventListener('click', removeItem)
-  });
+async function getProductData(id) {
+  return fetch("http://localhost:3000/api/products/" + id)
+    .then(res => res.json())
+    .then(function (response) {
+      return response;
+    })
+    .catch((error) => {
+      totalQty = '?';
+      totalPrice = '?';
+      header = (`Sacre Bleu!<p style=font-size:18px;>une erreur est survenue<br>merci de revenir plus tard</p></h1>`)
+      response = console.log("could not connect to item's API" + error);
+    })
 }
-eventListeners()
+displayCart()
+
+
+document.querySelectorAll('.itemQuantity').forEach(input => {
+  input.addEventListener('change', updateQty)
+});
+
+document.querySelectorAll(".deleteItem").forEach(p => {
+  p.addEventListener('click', removeItem)
+});
 
 
 // remove item if 'supprimer' is clicked
@@ -115,9 +80,9 @@ function removeItem(event) {
   getCart()
   let clicked = event.target.closest('article')
   console.log(clicked.dataset.id, clicked.dataset.color)
-  let tempCart = [];
+  const tempCart = [];
   for (let i = 0; i < cart.length; i++) {
-    let toDelete = cart.find(item => (item.id === clicked.dataset.id && item.color === clicked.dataset.color));
+    const toDelete = cart.find(item => (item.id === clicked.dataset.id && item.color === clicked.dataset.color));
     if (item != toDelete) {
       tempCart.push(item[qty, color, id])
     }
@@ -134,7 +99,7 @@ function removeItem(event) {
 // update item quantity if changed in input field
 async function updateQty(event) {
   await displayCart()
-  let newQty = event.target
+  const newQty = event.target
   console.log(newQty.value)
   if (newQty.value === 0) {
     alert("Merci de cliquez sur \"Supprimer\" pour retirer un article de votre panier")
@@ -146,7 +111,7 @@ async function updateQty(event) {
     const changed = newQty.closest('article')
     console.log(changed.dataset.id, changed.dataset.color)
     for (let item of cart) {
-      let hasNewQty = cart.find(item => (item.id === changed.dataset.id && item.color === changed.dataset.color));
+      const hasNewQty = cart.find(item => (item.id === changed.dataset.id && item.color === changed.dataset.color));
       if (item != hasNewQty) {
         tempCart.push(item[id, qty, color])
       }
@@ -165,7 +130,7 @@ async function updateQty(event) {
 
 const form = document.querySelector(".cart__order__form");
 
-var questions = document.querySelectorAll(".cart__order__form__question > input")
+let questions = document.querySelectorAll(".cart__order__form__question > input")
 console.log(questions)
 
 const firstName = document.getElementById("firstName")
@@ -284,20 +249,19 @@ email.addEventListener("input", () => {
 // When user submits data (check all for validity)
 form.addEventListener("submit", (event) => {
   event.preventDefault();
-  let validResponses = document.getElementsByClassName("valid")
-  console.log(validResponses.length)
-  if (validResponses.length = form.length) {
-    submitOrder()
+  const invalid = document.getElementsByClassName("invalid")
+  if (invalid.length >= 1) {
+    console.log("not all form values are valid")
   }
   else {
-    console.log("not all form values are valid")
+    submitOrder()
   }
 });
 
 //function runs if all form question inputs are valid
 /* send order information to API
 /* request order number in return */
-/*
+
 async function submitOrder() {
   const products = cart.map(item => item.id)
   const order = { contact, products }
@@ -310,10 +274,10 @@ async function submitOrder() {
     .then((response) => response.json())
     .then((response) => {
       console.log(response.orderId)
-      let baseURL = "./confirmation.html?order="
-      let confirmationURL = (baseURL + response.orderId)
+      const baseURL = "./confirmation.html?order="
+      const confirmationURL = (baseURL + response.orderId)
       console.log(confirmationURL)
       window.location = confirmationURL
     })
     .catch(error => console.error('Error:', error))
-}*/
+}
